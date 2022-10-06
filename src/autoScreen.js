dd38f2548ea430138aecd24dd9215e1d2cd8d8ef
yugutou <1220253798@qq.com>
2022-10-07 00:15:24 +0800

    111

--- src/autoScreen.js
+++ src/autoScreen.js
@@ -1,85 +1,86 @@
 // ==UserScript==
 // @name         bilibili/huya自动屏幕全屏原画Pro
 // @namespace    http://tampermonkey.net/
-// @version      2.0.4
+// @version      2.0.6
 // @description  bilibili/huya 500ms后自动屏幕全屏原画Pro
 // @author       wj
 // @match        https://www.huya.com/**
+// @match        https://www.huya.com/*
 // @match        https://live.bilibili.com/**
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @license      MIT
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+;(function () {
+  'use strict'
 
-     function pageOnload(targetNode, cb) {
-        // 创建一个观察器实例并传入回调函数
-        const observer = new MutationObserver(cb);
+  function pageOnload(targetNode, cb) {
+    // 创建一个观察器实例并传入回调函数
+    const observer = new MutationObserver(cb)
 
-        // 以上述配置开始观察目标节点
-        observer.observe(targetNode, { childList: true });
-    }
+    // 以上述配置开始观察目标节点
+    observer.observe(targetNode, { childList: true })
+  }
 
-    // Your code here...
-    /* ---------- BILIBILI ------------- */
-    // 改画质
-    function BiLiPlayerQuality() {
-        const ele = document.getElementById('live-player')
-        const mousemove = new Event("mousemove");
-        ele.dispatchEvent(mousemove);
+  // Your code here...
+  /* ---------- BILIBILI ------------- */
+  // 改画质
+  function BiLiPlayerQuality() {
+    const ele = document.getElementById('live-player')
+    const mousemove = new Event('mousemove')
+    ele.dispatchEvent(mousemove)
 
-        const qualityBtn = document.querySelector('.quality-wrap.svelte-73epzt');
-        const mouseenter = new Event("mouseenter");
-        qualityBtn.dispatchEvent(mouseenter);
-        setTimeout(() => {
-            const proBtn = document.querySelectorAll('.quality-it.svelte-73epzt')[0];
-            proBtn.click()
-            BiLiAutoOpen()
-        }, 100)
+    const qualityBtn = document.querySelector('.quality-wrap.svelte-73epzt')
+    const mouseenter = new Event('mouseenter')
+    qualityBtn.dispatchEvent(mouseenter)
+    setTimeout(() => {
+      const proBtn = document.querySelectorAll('.quality-it.svelte-73epzt')[0]
+      proBtn.click()
+      BiLiAutoOpen()
+    }, 100)
+  }
+  // 自动屏幕全屏
+  function BiLiAutoOpen() {
+    if (document.body.className === 'supportWebp player-full-win over-hidden') {
+      return
     }
-    // 自动屏幕全屏
-    function BiLiAutoOpen() {
-        if(document.body.className === 'supportWebp player-full-win over-hidden') {return}
-        const ele = document.getElementById('live-player')
-        const ev = new Event("dblclick");
-        ele.dispatchEvent(ev);
-    }
-    function BILI() {
-        pageOnload(document.body, BiLiPlayerQuality)
-    }
-    /* ---------- 虎牙 ------------- */
-    // 改画质
-    function HYPlayerQuality() {
-        document.getElementById('player-fullpage-btn').click()
-    }
-    // 自动屏幕全屏
-    function HYAutoOpen() {
-        document.querySelectorAll('.player-videotype-list li')[0].click()
-    }
-    function HY() {
-        HYAutoOpen()
-        HYPlayerQuality()
-    }
+    const ele = document.getElementById('live-player')
+    const ev = new Event('dblclick')
+    ele.dispatchEvent(ev)
+  }
+  function BILI() {
+    pageOnload(document.body, BiLiPlayerQuality)
+  }
+  /* ---------- 虎牙 ------------- */
+  // 改画质
+  function HYPlayerQuality() {
+    document.getElementById('player-fullpage-btn').click()
+  }
+  // 自动屏幕全屏
+  function HYAutoOpen() {
+    document.querySelectorAll('.player-videotype-list li')[0].click()
+  }
+  function HY() {
+    HYAutoOpen()
+    HYPlayerQuality()
+  }
 
-
-    // 执行
+  // 执行
+  window.addEventListener('load', () => {
     const currentUrl = window.location.host
-    console.log({currentUrl})
-    switch(currentUrl) {
-        case 'www.huya.com':
-            HY()
-            break;
-            ;
-        case 'live.bilibili.com':
-            BILI()
-            break;
-            ;
-        case 'www.douyu.com': {
-            // DY()
-            break;
-        }
+    console.log({ currentUrl })
+    switch (currentUrl) {
+      case 'www.huya.com':
+        HY()
+        break
+      case 'live.bilibili.com':
+        BILI()
+        break
+      case 'www.douyu.com': {
+        // DY()
+        break
+      }
     }
-})();
-
+  })
+})()
