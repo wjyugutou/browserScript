2d26e4ecb6301b2811d92298ec1095075757c87c
yugutou <1220253798@qq.com>
2022-10-06 23:34:24 +0800

    111

---
+++ src/autoScreen.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         bilibili/huya自动屏幕全屏原画Pro
+// @namespace    http://tampermonkey.net/
+// @version      2.0.1
+// @description  bilibili/huya 500ms后自动屏幕全屏原画Pro
+// @author       wj
+// @match        https://www.huya.com/*
+// @match        https://live.bilibili.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+    function pageOnload(targetNode, cb) {
+      // 创建一个观察器实例并传入回调函数
+      const observer = new MutationObserver(cb);
+
+      // 以上述配置开始观察目标节点
+      observer.observe(targetNode, { childList: true });
+  }
+
+  // Your code here...
+  /* ---------- BILIBILI ------------- */
+  // 改画质
+  function BiLiPlayerQuality() {
+      const ele = document.getElementById('live-player')
+      const mousemove = new Event("mousemove");
+      ele.dispatchEvent(mousemove);
+
+      const qualityBtn = document.querySelector('.quality-wrap.svelte-73epzt');
+      const mouseenter = new Event("mouseenter");
+      qualityBtn.dispatchEvent(mouseenter);
+      setTimeout(() => {
+          const proBtn = document.querySelectorAll('.quality-it.svelte-73epzt')[0];
+          proBtn.click()
+          BiLiAutoOpen()
+      }, 100)
+  }
+  // 自动屏幕全屏
+  function BiLiAutoOpen() {
+      const ele = document.getElementById('live-player')
+      const ev = new Event("dblclick");
+      ele.dispatchEvent(ev);
+  }
+  function BILI() {
+      pageOnload(document.body, BiLiPlayerQuality)
+  }
+  /* ---------- 虎牙 ------------- */
+  // 改画质
+  function HYPlayerQuality() {
+      document.getElementById('player-fullpage-btn').click()
+  }
+  // 自动屏幕全屏
+  function HYAutoOpen() {
+      document.querySelectorAll('.player-videotype-list li')[0].click()
+  }
+  function HY() {
+      HYAutoOpen()
+      HYPlayerQuality()
+  }
+
+  // 执行
+  window.onload = function () {  
+    setTimeout(() => {
+        const currentUrl = window.location.host
+        switch(currentUrl) {
+            case 'www.huya.com':
+                HY()
+                break;
+                case 'live.bilibili.com':
+                BILI()
+                break;
+                case 'www.douyu.com': 
+                // DY()
+                break;
+            }
+        }, 1000)
+  }
+})();
